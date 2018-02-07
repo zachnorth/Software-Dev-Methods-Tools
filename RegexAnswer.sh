2e27a9451c573787bca52eca33daa3070aa85e1d
adam casey <adam-casey@live.com>
2018-02-07 16:16:24 -0700

    Update RegexAnswer.sh
    Answers to questions 4, 5, and 7!

--- RegexAnswer.sh
+++ RegexAnswer.sh
@@ -6,19 +6,24 @@ fileName="$1"
 printf "FileName: %s\n" $fileName
 
 
-# 1. How many lines end with a number? Answer 15371
+# 1. How many lines end with a number? Answer = 15371
 grep -e "[0-9]$" $fileName | wc -l
 
-# 2. How many lines do not start with a vowel? Should be 52331 or 52332
-egrep -e "^[^aeiouAEIOU]" $fileName | wc -l
+# 2. How many lines do not start with a vowel? Answer = 52331 
+grep -e "^[^aeiouAEIOU]" $fileName | wc -l
 
 # 3. How many 12 letter (alphabet only) lines?
-# 4. How many phone numbers are in the dataset (format: ‘_ _ _-_ _ _-_ _ _ _’)?
-# 5. How many city of Boulder phone numbers (Eg: starting with 303-_ _ _-_ _ _ _)?
-# 6. How many begin with a vowel and end with a number? 
-# 7. How many email addresses are from geocities? (Eg: end with'geocities.com')?
-# 8. How many email addresses are in ‘first.last’ name format and involve someone who’s first name starts with a letter in the first half of the alphabet? 
-# while read -r line; do
-# 	echo $line
 
-# done < "$1"
+# 4. How many phone numbers are in the dataset (format: ‘_ _ _-_ _ _-_ _ _ _’)? Answer = 767
+grep -e "^[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}$" $fileName | wc -l
+
+# 5. How many city of Boulder phone numbers (Eg: starting with 303-_ _ _-_ _ _ _)? Answer = 406
+grep -e "^[303]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}$" $fileName | wc -l
+
+# 6. How many begin with a vowel and end with a number? 
+
+
+# 7. How many email addresses are from geocities? (Eg: end with'geocities.com')? Answer 6619
+grep 'geocities.com$' $fileName | wc -l
+
+# 8. How many email addresses are in ‘first.last’ name format and involve someone who’s first name starts with a letter in the first half of the alphabet? 
