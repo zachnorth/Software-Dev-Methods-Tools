f958458272c5a6d99e31ee777411ec588f450311
adam casey <adam-casey@live.com>
2018-02-10 16:28:22 -0700

    Update GradesAwk.sh
    Added print conditions for empty command line but it shouldn't be printing out in decimal form

--- GradesAwk.sh
+++ GradesAwk.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
 
+if [ -z "$1" ]; then
+	printf "Usage: GradesAwk.sh filename\n"
 
-#copy and paste your code here
+else
+
+awk '{sum=0; n=0; for(i=4;i<=NF;i++) {sum+=$i; ++n} print sum/n, "[" $1 "] " $3 "," $2}' $1 | sort -k3 -k1
+
+fi
