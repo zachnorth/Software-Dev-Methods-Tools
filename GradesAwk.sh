d1c1db374a3641a3454b6190ad60c76ce7556433
zachnorth <36344399+zachnorth@users.noreply.github.com>
2018-02-10 17:14:57 -0700

    Update GradesAwk.sh

--- GradesAwk.sh
+++ GradesAwk.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
+#Adam Casey and Zach North
 
-if [ -z "$1" ]; then
-	printf "Usage: GradesAwk.sh filename\n"
-
-else
-
-awk '{sum=0; n=0; for(i=4;i<=NF;i++) {sum+=$i; ++n} print sum/n, "[" $1 "] " $3 "," $2}' $1 | sort -k3 -k1
-
-fi
+#!/bin/sh
+awk '{sum=0; n=0; average=0; for(i=4;i<=NF;i++) {sum+=$i; ++n} {average=int(sum/n)} 
+print average, "[" $1 "] " $3 "," $2}' $1 | sort -k3 -k1
